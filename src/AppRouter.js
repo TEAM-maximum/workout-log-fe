c96ef6784ae7b8c506001fd04145e29f97c42be0
dda5gi <ddk03121@naver.com>
2022-04-04 19:48:34 +0900

    프로젝트 시작

---
+++ src/AppRouter.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./index.css";
+import App from "./App";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const AppRouter = () => {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/login" element={<Login />}>
+            </Route>
+            <Route path="/signup" element={<SignUp />}>
+            </Route>
+            <Route path="/" element={<App />}>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default AppRouter;
