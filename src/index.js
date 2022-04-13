48b56f917aa0f2811ad93581afff5e21899e5629
dda5gi <ddk03121@naver.com>
2022-04-14 01:47:57 +0900

    Feat: 폰트 추가

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import AppRouter from "./AppRouter";
 import reportWebVitals from "./reportWebVitals";
+import "./static/fonts/fonts.css";
+
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Hahmlet',
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppRouter />
+    <MuiThemeProvider theme={theme}>
+      <AppRouter />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
